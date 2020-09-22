59c5f14edc48f761aafa3c7b3ad41ac3a7eb71fa
Sergey <Kadochnikov91@yandex.ru>
2020-09-22 23:00:18 +0300

    Первый коммит.
    Подключил фронт. Создайл файл application.yml. Создал контроллеры. Подключил зависимости в pom.xml. Создал Main класс. Приложение запускается, шаблон index.html открывается.

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,19 @@
+package application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Main class of the application
+ * @author Sergey Kadochnikov
+ * @version 1.0
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    SpringApplication.run(Application.class, args);
+
+    }
+}
