f0a261316a75afd716e7417c00a3d1a00bbb93fb
Sergey <Kadochnikov91@yandex.ru>
2020-09-23 13:13:16 +0300

    Первый коммит.
    Подключил фронт. Создайл файл application.yml. Создал контроллеры. Подключил зависимости в pom.xml. Создал Main класс. Приложение запускается, шаблон index.html открывается.

--- src/main/java/application/controller/DefaultController.java
+++ src/main/java/application/controller/DefaultController.java
@@ -3,7 +3,12 @@ package application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * A public class that describes controllers for non API requests
+ * @author Sergey Kadochnikov
+ * @version 1.0
+ */
 
 @Controller
 public class DefaultController {
