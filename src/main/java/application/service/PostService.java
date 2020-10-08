514c141b388e6637387fbc9ba3ee0ad8d36c36f0
Sergey <Kadochnikov91@yandex.ru>
2020-10-08 15:05:43 +0300

    реализовал контроллеры к запросам с главной страницы(без запросов к БД).Добавил предварительно dto объекты для ответов на сервер. Подключил миграцию БД с flyway, создал первую версию скрипта.

--- src/main/java/application/service/PostService.java
+++ src/main/java/application/service/PostService.java
@@ -13,20 +13,16 @@ import java.util.List;
 @Service
 public class PostService {
 
-    
+
     public PostResponseDTO getPosts() {
         PostResponseDTO responseDTO = new PostResponseDTO();
         responseDTO.setCount(2);
-        UserDTO userDTO = new UserDTO(1,"Some User");
-        PostDTO postDTO = new PostDTO(1,System.currentTimeMillis(),userDTO,
-                "New Post","Text Text Text Text",
-                5,1,0,9);
-        PostDTO postDTO2 = new PostDTO(2,System.currentTimeMillis(),userDTO,
-                "New Post2","Text Text Text Text",
-                3,1,4,9);
+        UserDTO userDTO = new UserDTO(1, "Some User");
         List<PostDTO> postDTOS = new ArrayList<>();
-        postDTOS.add(postDTO);
-        postDTOS.add(postDTO2);
+        for (int i = 0; i < 15; i++) {
+            PostDTO postDTO = new PostDTO(i, System.currentTimeMillis(), userDTO, "New Post " + i, "SomeTest", i, 1, 3, 10);
+            postDTOS.add(postDTO);
+        }
         responseDTO.setPosts(postDTOS);
         return responseDTO;
     }
